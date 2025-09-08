502196af4bd03383d6fe58fed7bef11d8ddaad8b
@Masum0895 <2221793@iub.edu.bd>
2025-09-09 01:44:54 +0600

    Backend Completed

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import 'dotenv/config'
 import songRouter from './src/routes/songRoute.js';
 import connectDB from './src/config/mongodb.js';
 import connectCloudinary from './src/config/cloudinary.js';
+import albumRouter from './src/routes/albumRoute.js';
 
 //app config
 const app = express();
@@ -18,5 +19,8 @@ app.use(cors()); // To connect frontend with backend
 
 // initializing routes
 app.use('/api/song',songRouter);
+
+app.use('/api/album',albumRouter);
+
 app.get('/',(req,res)=>res.send("API Working"));
 app.listen(port,()=>console.log(`Server started on ${port}`));
